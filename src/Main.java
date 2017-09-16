eb821eba3095c60af71ad05a8570be9f49630ce4
HimaPoornima <32011063+HimaPoornima@users.noreply.github.com>
2017-09-16 15:26:04 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) 
+        {
+       
+  
+           Scanner sc=new Scanner(System.in);
+           int n=sc.nextInt();
+           Student s=new Student(n);
+           
+           StudentGroup s=new StudentGroup(n);
+                 
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
